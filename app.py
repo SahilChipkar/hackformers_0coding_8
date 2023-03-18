921e2ee89203e05fdfe73baf757d8e6f7d45ebcf
Dongre Kunal <95275443+dongrekunal@users.noreply.github.com>
2023-03-19 04:16:24 +0530

    Merged model and verification

--- app.py
+++ app.py
@@ -1,45 +1,46 @@
+
 import requests
 import pytesseract
 import re 
 import json
 from PIL import Image
-from flask import Flask, render_template
-
-img_path = 'img1.jpeg'
-
-extracted_info = pytesseract.image_to_string(Image.open(img_path))
-gst_pattern = r"\b\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d[Z,z,2]{1}\d{1}\b"
-gst_number = re.search(gst_pattern, extracted_info).group(0)
-# print(gst_number)
+from flask import Flask, render_template, request
+from io import BytesIO
 
 app = Flask(__name__)
+
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
+@app.route('/process_image', methods=['POST'])
+def checking():
+    img_file = request.files['image']
+    img = Image.open(BytesIO(img_file.read()))
+    extracted_info = pytesseract.image_to_string(img)
+    # print(extracted_info)
+    gst_pattern = r"\b\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d[Z,z,2]{1}\d{1}\b"
+    gst_number = re.search(gst_pattern, extracted_info).group(0)
+    u_gst = gst_number[:13] + "Z" + gst_number[-1]
+    print(u_gst)
 
-
-@app.route('/check')
-def home():
-   
     headers = {
-        "Content-Type": "application/json",
-        "X-RapidAPI-Key": "834dfdf592mshe0ed34b5a181dfbp1a2eafjsn91d66a151700",
-        "X-RapidAPI-Host": "gst-return-status.p.rapidapi.com"
+	"Content-Type": "application/json",
+	"X-RapidAPI-Key": "22886f282fmsh7f05dcaead6e345p16754ejsnda3b65078829",
+	"X-RapidAPI-Host": "gst-return-status.p.rapidapi.com"
     }
+
+
     
-    
-    check_url= f"https://gst-return-status.p.rapidapi.com/free/gstin/{gst_number}"
-    
+    check_url= f"https://gst-return-status.p.rapidapi.com/free/gstin/{u_gst}"
     
     response = requests.get(check_url, headers=headers)
     
-   
     a = json.loads(response.text)
     if a['success']:
         return render_template('verifi.html')
     else:
         return "<h2>Store on blockchain</h2>"
-
+   
 if __name__ == '__main__':
     app.run(debug=True)
