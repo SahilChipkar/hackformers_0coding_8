25386b75ca3759d238b3cae8e0cef9083099b800
Dongre Kunal <95275443+dongrekunal@users.noreply.github.com>
2023-03-19 06:44:50 +0530

    Feature addon
    Send user to retry if image upload is not visible

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import requests
 import pytesseract
 import re 
@@ -20,27 +19,33 @@ def checking():
     extracted_info = pytesseract.image_to_string(img)
     # print(extracted_info)
     gst_pattern = r"\b\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d[Z,z,2]{1}\d{1}\b"
-    gst_number = re.search(gst_pattern, extracted_info).group(0)
-    u_gst = gst_number[:13] + "Z" + gst_number[-1]
-    print(u_gst)
+    match = re.search(gst_pattern, extracted_info)
+    if match:
+        gst_number = match.group(0)
+        # process gst_number
+        u_gst = gst_number[:13] + "Z" + gst_number[-1]
+        # print(u_gst)
 
-    headers = {
-	"Content-Type": "application/json",
-	"X-RapidAPI-Key": "22886f282fmsh7f05dcaead6e345p16754ejsnda3b65078829",
-	"X-RapidAPI-Host": "gst-return-status.p.rapidapi.com"
-    }
+        headers = {
+        "Content-Type": "application/json",
+        "X-RapidAPI-Key": "22886f282fmsh7f05dcaead6e345p16754ejsnda3b65078829",
+        "X-RapidAPI-Host": "gst-return-status.p.rapidapi.com"
+        }
 
 
+        
+        check_url= f"https://gst-return-status.p.rapidapi.com/free/gstin/{u_gst}"
+        
+        response = requests.get(check_url, headers=headers)
+        
+        a = json.loads(response.text)
+        if a['success']:
+            return render_template('verifi.html',a=a)
+        else:
+            return "<h2>Store on blockchain</h2>"
     
-    check_url= f"https://gst-return-status.p.rapidapi.com/free/gstin/{u_gst}"
-    
-    response = requests.get(check_url, headers=headers)
-    
-    a = json.loads(response.text)
-    if a['success']:
-        return render_template('verifi.html',a=a)
     else:
-        return "<h2>Store on blockchain</h2>"
+        return render_template('retry.html')
    
 if __name__ == '__main__':
     app.run(debug=True)
